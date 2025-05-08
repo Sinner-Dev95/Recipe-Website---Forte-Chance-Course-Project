26a605e209f89df90a5692df29517385fd40a663
Sinner-Dev95 <ID+username@users.noreply.github.com>
2025-05-08 17:46:56 +0200

    Fixed hamburger menu and static menu for desktop

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Definizione delle variabili
   const hamburgerIcon = document.querySelector('.hamburger-icon');
   const headerMenu = document.querySelector('.header-menu');
   const menuItems = document.querySelectorAll('.menu-item');
   const header = document.querySelector('.header');
-
-  hamburgerIcon.addEventListener('touchstart', function() {
+  
+  // Aggiungi sia touchstart che click per una migliore compatibilità cross-device
+  hamburgerIcon.addEventListener('touchstart', toggleMenu);
+  hamburgerIcon.addEventListener('click', toggleMenu);
+  
+  function toggleMenu(event) {
+    console.log('Menu toggle triggerato!');
+    event.preventDefault(); // Previene comportamenti indesiderati
+    
+    // Toggle delle classi sui vari elementi
+    hamburgerIcon.classList.toggle('open');
     headerMenu.classList.toggle('open');
-    header.classList.toggle('open');
+    
+    // Aggiungi la variabile --item-index a ciascun elemento menu-item
     menuItems.forEach((item, index) => {
-      setTimeout(() => {
-        item.classList.toggle('open');
-      }, index * 50);
+      item.style.setProperty('--item-index', index);
+      item.classList.toggle('open');
     });
-  });
+  }
 });
